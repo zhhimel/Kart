09965284ec7c4f56d705c6656beae1f3753d5575
ZH-Himel <zh.himel.2002@gmail.com>
2023-06-13 21:14:20 +0600

    first commit and set navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function Navbars() {
+    return (
+        <Navbar bg="light" variant="light">
+            <Container>
+                <div className='d-flex align-items-center'>
+                    <Navbar.Brand href="#home">
+                        <img
+                            alt=""
+                            src="/img/logo.jpg"
+                            width="30"
+                            height="30"
+                        />
+                    </Navbar.Brand>
+                    <Navbar.Brand href="#home">KART</Navbar.Brand>
+                </div>
+
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#categories">Categories</Nav.Link>
+                    <Nav.Link href="#contact">Contact</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+
+    );
+}
+
+export default Navbars;
